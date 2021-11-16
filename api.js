2cba28bed1a28974641f4a4c5401934c5de3c6f1
Onionie <gutierrez.onionie@gmail.com>
2021-11-15 23:43:31 -0800

    Map Displaying, Can't Hide API key

---
+++ api.js
@@ -0,0 +1,3 @@
+alert("working");
+require("dotenv").config();
+const API_Key = process.env.API_KEY;
--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-alert("Working");
-require("dotenv").config();
+require(["esri/config", "esri/Map", "esri/views/MapView"], function (
+  esriConfig,
+  Map,
+  MapView
+) {
+  //prettier-ignore
+  esriConfig.apiKey ="AAPK8901ebc02b514937915f81d78ec61b7fStC7-kmQA7QW0y3Ddu1E7ZvmwKgCxO96JJ9Mbu83t21pWiR8a_K3930gRi0EMXqg";
 
-const apiKey = process.env.API_KEY;
+  const map = new Map({
+    basemap: "arcgis-topographic", // Basemap layer service
+  });
+
+  const view = new MapView({
+    map: map,
+    center: [-117.1803488, 34.1261445], // Longitude, latitude
+    zoom: 13, // Zoom level
+    container: "viewDiv", // Div element
+  });
+});
