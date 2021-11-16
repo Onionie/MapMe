04c08c4f979b96f08729b797774a9a0df48e1684
Onionie <gutierrez.onionie@gmail.com>
2021-11-16 00:43:11 -0800

    Added Find places feature

--- script.js
+++ script.js
@@ -43,11 +43,90 @@ require([
     view: view,
     useHeadingEnabled: false,
     goToOverride: function (view, options) {
-      options.target.scale = 2500;
+      options.target.scale = 30000;
       return view.goTo(options.target);
     },
   });
 
   //Position feature top left
   view.ui.add(locate, "top-left");
-});
+
+  const places = [
+    "Choose a place type...",
+    "Parks and Outdoors",
+    "Coffee shop",
+    "Gas station",
+    "Food",
+    "Hotel",
+  ];
+
+  const select = document.createElement("select", "");
+  select.setAttribute("class", "esri-widget esri-select");
+  select.setAttribute(
+    "style",
+    "width: 175px; font-family: 'Avenir Next W00'; font-size: 1em"
+  );
+
+  places.forEach(function (p) {
+    const option = document.createElement("option");
+    option.value = p;
+    option.innerHTML = p;
+    select.appendChild(option);
+  });
+
+  view.ui.add(select, "top-right");
+
+  //prettier-ignore
+  const locatorUrl ="http://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer";
+
+  // Find places and add them to the map
+  function findPlaces(category, pt) {
+    locator
+      .addressToLocations(locatorUrl, {
+        location: pt,
+        categories: [category],
+        maxLocations: 25,
+        outFields: ["Place_addr", "PlaceName"],
+      })
+
+      .then(function (results) {
+        view.popup.close();
+        view.graphics.removeAll();
+
+        results.forEach(function (result) {
+          view.graphics.add(
+            new Graphic({
+              attributes: result.attributes, // Data attributes returned
+              geometry: result.location, // Point returned
+              symbol: {
+                type: "simple-marker",
+                color: "#000000",
+                size: "12px",
+                outline: {
+                  color: "#ffffff",
+                  width: "2px",
+                },
+              },
+
+              popupTemplate: {
+                title: "{PlaceName}", // Data attribute names
+                content: "{Place_addr}",
+              },
+            })
+          );
+        });
+      });
+  }
+
+  // Search for places in center of map
+  view.watch("stationary", function (val) {
+    if (val) {
+      findPlaces(select.value, view.center);
+    }
+  });
+
+  // Listen for category changes and find places
+  select.addEventListener("change", function (event) {
+    findPlaces(event.target.value, view.center);
+  });
+}); //End of require
