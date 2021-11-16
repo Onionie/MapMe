5eb78765462c2b9441fec233b5b5cd6418ce26e6
Onionie <gutierrez.onionie@gmail.com>
2021-11-16 00:27:30 -0800

    Basemap layer added

--- script.js
+++ script.js
@@ -5,7 +5,18 @@ require([
   "esri/rest/locator",
   "esri/Graphic",
   "esri/widgets/Locate",
-], function (esriConfig, Map, MapView, locator, Graphic, Locate) {
+  "esri/widgets/BasemapToggle",
+  "esri/widgets/BasemapGallery",
+], function (
+  esriConfig,
+  Map,
+  MapView,
+  locator,
+  Graphic,
+  Locate,
+  BasemapToggle,
+  BasemapGallery
+) {
   //prettier-ignore
   esriConfig.apiKey ="AAPK8901ebc02b514937915f81d78ec61b7fStC7-kmQA7QW0y3Ddu1E7ZvmwKgCxO96JJ9Mbu83t21pWiR8a_K3930gRi0EMXqg";
 
@@ -20,13 +31,23 @@ require([
     container: "viewDiv", // Div element
   });
 
+  //Basemap Layer Feature
+  const basemapToggle = new BasemapToggle({
+    view: view,
+    nextBasemap: "arcgis-imagery",
+  });
+  view.ui.add(basemapToggle, "bottom-right");
+
+  //Locate feature
   const locate = new Locate({
     view: view,
     useHeadingEnabled: false,
     goToOverride: function (view, options) {
-      options.target.scale = 3000;
+      options.target.scale = 2500;
       return view.goTo(options.target);
     },
   });
+
+  //Position feature top left
   view.ui.add(locate, "top-left");
 });
